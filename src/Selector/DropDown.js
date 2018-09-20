b97142882b4effe9c7e5d7cd08a8b70fabb60263
Balázs Orbán <info@balazsorban.com>
2018-09-20 10:32:21 +0200

    🚨 eslint formatting

--- src/Selector/DropDown.js
+++ src/Selector/DropDown.js
@@ -1,29 +1,42 @@
-import React, { Component, Fragment } from 'react';
-import { withStore } from '../Store';
+import React, {Component, Fragment} from 'react'
+import "./Selector.css"
+import {withStore} from '../Store'
 
 class DropDown extends Component {
 
-    render() { 
-        const {handleDropDown, types, selected, categories, labels} = this.props
-        return (
-            <div>
-                {types.map(({id, name}) => 
-                <Fragment>
-                    <label htmlFor={name}>{labels[name]}</label>
-                        <select
-                            key={id}
-                            value={categories[name].find(category => category.name === selected[name])} 
-                            name={name} 
-                            id={name}
-                            onChange={handleDropDown}>
-                            {categories[name].map(({id, name}) => <option key={id} value={id}>
-                            {name}
-                        </option> ) }
-                    </select>
-                </Fragment>
-                )}
-            </div> 
-        );
-    }
+  render() {
+    const {
+      handleDropDown, types, selected, categories, labels
+    } = this.props
+    return (
+      <div className="dropdowns">
+        {types.map(({
+          id, name
+        }) =>
+          <Fragment>
+            <label htmlFor={name}>{labels[name]}</label>
+            <select
+              id={name}
+              key={id}
+              name={name}
+              onChange={handleDropDown}
+              value={categories[name].find(category => category.name === selected[name])}
+            >
+              {categories[name].map(({
+                id, name
+              }) =>
+                <option
+                  key={id}
+                  value={id}
+                >
+                  {name}
+                </option>
+              )}
+            </select>
+          </Fragment>
+        )}
+      </div>
+    )
+  }
 }
-export default withStore(DropDown);
+export default withStore(DropDown)
